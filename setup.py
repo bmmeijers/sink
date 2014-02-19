428d094bbaeeefc4a2c326935a79c6c13fcaef9d
Martijn Meijers <b.m.meijers@tudelft.nl>
2014-02-19 14:51:52 +0100

    Adding new setup files

--- setup.py
+++ setup.py
@@ -1,18 +1,14 @@
-import sys
+import ez_setup
+ez_setup.use_setuptools()
+from setuptools import setup, find_packages, Extension
 import os
-
-from setuptools import setup
 
 def get_version():
     """
     Gets the version number. Pulls it from the source files rather than
     duplicating it.
-    
     """
-    # we read the file instead of importing it as root sometimes does not
-    # have the cwd as part of the PYTHONPATH
     fn = os.path.join(os.path.dirname(__file__), 'src', 'sink', '__init__.py')
-    
     try:
         lines = open(fn, 'r').readlines()
     except IOError:
@@ -29,13 +25,36 @@ def get_version():
                            "'__version__ =' string not found")
     return version
 
+PACKAGES = find_packages('src')
+EXT_MODULES = []
+SCRIPTS = [] 
+REQUIREMENTS = []
+DATA_FILES = []
+
 setup(
     name = "sink",
     version = get_version(),
+    packages = PACKAGES,
+    package_dir = {"": "src"},
     author = "Martijn Meijers",
-    author_email = "b dot m dot meijers at tudelft dot nl",
-    license = "MIT",
+    author_email = "b.m.meijers@tudelft.nl",
     description = "A lightweight abstraction for Table Oriented Output",
-    url = "",
-    package_dir = {'':'src'},
+    url = "https://bitbucket.org/bmmeijers/sink/",
+    license = "MIT license",
+    ext_modules = EXT_MODULES,
+    data_files = DATA_FILES,
+    zip_safe = False,
+    scripts = SCRIPTS,
+    install_requires = REQUIREMENTS,
+    classifiers = [
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
 )
